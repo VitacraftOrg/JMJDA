e757f0f0553b6e50cc5916c1e9be84bfc3f23c7a
Relism <itzdrekkopvp@gmail.com>
2024-04-27 01:54:16 +0200

    agagag :3

--- src/main/java/net/vitacraft/jmjda/api/util/log.java
+++ src/main/java/net/vitacraft/jmjda/api/util/log.java
@@ -1,10 +1,15 @@
 package net.vitacraft.jmjda.api.util;
 
+import net.vitacraft.jmjda.api.config.ConfigUtil;
+
 public class log {
+    static ConfigUtil config = new ConfigUtil("config.yml");
     public static void console(String message){
         System.out.println(message);
     }
     public static void debug(String message){
-        console("[DEBUG] > " + message);
+        if(config.getBool("debug")){
+            console("[DEBUG] > " + message);
+        }
     }
 }
