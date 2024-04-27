d566b91c5a8997647c185fd88ece3a2b8d18ede6
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-04-27 03:04:40 +0200

    oops

--- src/main/java/net/vitacraft/jmjda/api/util/log.java
+++ src/main/java/net/vitacraft/jmjda/api/util/log.java
@@ -6,9 +6,11 @@ import net.vitacraft.jmjda.api.config.Filetype;
 
 public class log {
     static Config config = ConfigUtil.getConfig("config.yml", Filetype.YAML);
+
     public static void console(String message){
         System.out.println(message);
     }
+
     public static void debug(String message){
         if(config.getBool("debug")){
             console("[DEBUG] > " + message);
